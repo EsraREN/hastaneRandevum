5a57e77bfdd1de5516d2b8fbf634484bbc8bc94e
EsraREN <119942465+EsraREN@users.noreply.github.com>
2024-03-10 13:49:11 +0300

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,1 +1,27 @@
-G171210021
+package paket;
+/**
+*
+* @author Fatih
+*/
+public class Sayi {
+	private int deger;
+	private boolean eksimi;
+	
+	public Sayi(int deger)
+	{
+		this.deger = deger;
+		eksimi = deger < 0;
+	}
+	public void setDeger(int value) {
+		this.deger = value;
+		eksimi = this.deger < 0;
+	}
+	public boolean Eksimi(){
+		return eksimi;
+	}
+	
+	@Override
+	public String toString() {
+		return String.valueOf(deger);
+	}
+}
